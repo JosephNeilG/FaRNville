ffaa3e7b4517d7eb6fabaaff740582abb36599ea
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-09-24 11:06:12 +0800

    Build AddPlant card

---
+++ components/shop/AddPlantCard.tsx
@@ -0,0 +1,17 @@
+import { COLORS } from "@/constants/Colors";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+const AddPlantCard = () => {
+	return (
+		<TouchableOpacity className="h-[110px] rounded-xl mt-4 border-2 border-dashed border-dark-300 items-center justify-center">
+			<FontAwesome6 name="leaf" size={22} color={COLORS.primary} />
+			<Text className="text-dark-300 text-xl font-regular mt-1">
+				Add a Plant
+			</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default AddPlantCard;
