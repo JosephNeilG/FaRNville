b5b2c295b5bbd95c0d55de36325bdae2b9817c41
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-10-02 17:31:59 +0800

    Fix width issue

--- components/CustomModal.tsx
+++ components/CustomModal.tsx
@@ -1,12 +1,5 @@
 import React, { ReactNode } from "react";
-import {
-	GestureResponderEvent,
-	Modal,
-	Platform,
-	Pressable,
-	View,
-	useWindowDimensions,
-} from "react-native";
+import { GestureResponderEvent, Modal, Pressable, View } from "react-native";
 
 interface CustomModalProps {
 	is_open: boolean;
@@ -15,11 +8,6 @@ interface CustomModalProps {
 }
 
 const CustomModal = ({ is_open, onClose, children }: CustomModalProps) => {
-	const { width } = useWindowDimensions();
-
-	const modal_width =
-		Platform.OS === "web" ? Math.min(width * 0.9, 600) : "90%";
-
 	const handleBackdropPress = (event: GestureResponderEvent) => {
 		if (event.target === event.currentTarget) onClose();
 	};
@@ -31,7 +19,10 @@ const CustomModal = ({ is_open, onClose, children }: CustomModalProps) => {
 				className="flex-1 justify-center items-center bg-[rgba(0,0,0,0.5)]">
 				<View
 					className="bg-light-100 p-5 rounded-xl items-center"
-					style={{ width: modal_width }}>
+					style={{
+						width: "90%",
+						maxWidth: 600,
+					}}>
 					{children}
 				</View>
 			</Pressable>
--- components/ListContainer.tsx
+++ components/ListContainer.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import {
-	DimensionValue,
-	Platform,
-	View,
-	useWindowDimensions,
-} from "react-native";
+import { View } from "react-native";
 
 const ListContainer = ({ children }: { children: React.ReactNode }) => {
-	const { width } = useWindowDimensions();
-
-	let container_width: DimensionValue = "100%";
-
-	if (Platform.OS === "web") {
-		if (width > 0) {
-			container_width = Math.min(width * 0.9, 600);
-		} else {
-			container_width = "100%";
-		}
-	}
-
 	return (
 		<View
 			style={{
-				width: container_width,
+				width: "100%",
+				maxWidth: 600,
 				alignSelf: "center",
 				flex: 1,
 			}}>
